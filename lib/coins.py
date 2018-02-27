7934660cc6521b0db417d2c91542c8a87c05c4a4
Chris Sulmone <csulmone@gmail.com>
2018-02-26 18:38:46 -0800

    Added BitcoinPrivate mainnet and testnet

--- lib/coins.py
+++ lib/coins.py
@@ -919,6 +919,40 @@ class ZclassicTestnet(EquihashMixin, Coin):
     REORG_LIMIT = 800
     CHUNK_SIZE = 200
 
+class BitcoinPrivate(EquihashMixin, Coin):
+    NAME = "BitcoinPrivate"
+    SHORTNAME = "BTCP"
+    NET = "mainnet"
+    P2PKH_VERBYTE = bytes.fromhex("1325")
+    P2SH_VERBYTES = [bytes.fromhex("13AF")]
+    WIF_BYTE = bytes.fromhex("80")
+    GENESIS_HASH         = ( '0007104ccda289427919efc39dc9e4d4'
+                             '99804b7bebc22df55f8b834301260602')
+    DESERIALIZER = lib_tx.DeserializerZcash
+    TX_COUNT = 329196
+    TX_COUNT_HEIGHT = 68379
+    TX_PER_BLOCK = 5
+    RPC_PORT = 7932
+    REORG_LIMIT = 800
+    CHUNK_SIZE = 200
+
+class BitcoinPrivateTestnet(EquihashMixin, Coin):
+    NAME = "BitcoinPrivate"
+    SHORTNAME = "BTCP"
+    NET = "testnet"
+    P2PKH_VERBYTE = bytes.fromhex("1958")
+    P2SH_VERBYTES = [bytes.fromhex("19E0")]
+    WIF_BYTE = bytes.fromhex("EF")
+    GENESIS_HASH         = ( '03e1c4bb705c871bf9bfda3e74b7f8f8'
+                             '6bff267993c215a89d5795e3708e5e1f')
+    DESERIALIZER = lib_tx.DeserializerZcash
+    TX_COUNT = 329196
+    TX_COUNT_HEIGHT = 68379
+    TX_PER_BLOCK = 5
+    RPC_PORT = 17932
+    REORG_LIMIT = 800
+    CHUNK_SIZE = 200
+
 class Komodo(KomodoMixin, EquihashMixin, Coin):
     NAME = "Komodo"
     SHORTNAME = "KMD"
